a1ba05a5f274fa7b2be01605960bd43e95df7182
code@localhost <code@localhost>
2024-12-16 19:28:07 -0500

    fixed syntax in data_to_json.js

--- data_to_json.js
+++ data_to_json.js
@@ -1,9 +1,9 @@
-let entries = document.body.textContent.split("\n").map(x=>JSON.parse(x)) -- input from text file document
-let systems = {}
+let entries = document.body.textContent.split("\n").map(x=>JSON.parse(x)); // input from text file document
+let systems = {};
 entries.forEach(entry=>{
 	for(coordinate in entry){
 		systems[coordinate] = entry[coordinate]
 	}
-})
-console.log(Object.keys(systems).length)
-copy(systems) -- output to clipboard
+});
+console.log(Object.keys(systems).length);
+copy(systems); // output to clipboard
